83f4b5104a59dccc985e55d4946c4133efa34f88
Paul Vincent <pvincent2@UL03MBOCA-01.local>
2020-10-23 09:35:18 +0100

    Added NS-Cloner Endpoint
    Added /clone endpoint, referencing the NS Cloner plugin.

--- multisite-rest-api.php
+++ multisite-rest-api.php
@@ -41,6 +41,20 @@ add_action( 'rest_api_init', function () {
   ]);
 });
 
+
+add_action( 'rest_api_init', function () {
+  $now = current_time( 'mysql', true );
+  register_rest_route('wp/v2', '/sites/clone', [
+    'methods'  => "POST",
+    'callback' => 'wmra_sites_callback',
+    'args' => [
+        'domain',
+        'source',
+  		'title', 
+    ],
+  ]);
+});
+
 add_action( 'rest_api_init', function () {
   $now = current_time( 'mysql', true );
   register_rest_route('wp/v2', '/sites/assign', [
@@ -76,6 +90,23 @@ add_action( 'rest_api_init', function () {
   ]);
 });
 /* End Site Route setup */
+
+/**
+ * Before doing anything: set up clone request data.
+ * These are the same fields that get submitted via an AJAX request when cloning
+ * via the admin interface, so you can inspect that request to determine other
+ * ways to configure the parameters, particularly if you're using NS Cloner Pro
+ * and have more options available.
+ * $request = array(
+ *     'clone_mode'     => 'core',
+ *     'source_id'      => 1, // any blog/site id on network
+ *     'target_name'    => 'subdomain-or-subdir,
+ *     'target_title'   => 'New Site Title',
+ *     'debug'          => 1 // optional: enables logs
+ * }
+ */
+
+
 
 /* Begin Sites Callback */
 function wmra_sites_callback( $request ) {
@@ -137,6 +168,98 @@ function wmra_sites_callback( $request ) {
           return rest_ensure_response(wpmu_create_blog($domain, $path, $title, $user_id, $options));
         }
       }
+        
+        
+        // Edits below
+        
+        elseif (preg_match("/sites\/clone/", $route, $matches)) {
+        if(! current_user_can('create_sites')){
+          return rest_ensure_response('Invalid user permissions.');
+          die();
+        } else {
+          if(! $params["domain"]) {
+            return rest_ensure_response('You must provide a domain.');
+            die();
+          } else {
+            $target_name = $params["domain"];
+          }
+          if (! $params["title"]) {
+            $title = $domain;
+          } else {
+            $title = $params["title"];
+          }
+          if (! $params["source"]) {
+            $source_id = 2;
+          } else {
+            $source_id = $params["source"];
+          }
+            
+                   $request = array(
+      'clone_mode'     => 'core',
+      'action'         => 'process',                   
+      'source_id'      => $source_id, // any blog/site id on network
+      'target_name'    => $target_name,
+      'target_title'   => $title,
+      'disable_addons' => false,
+      'clone_nonce'    => wp_create_nonce('ns_cloner'),
+      'do_copy_posts'  => 1, 
+      'debug'          => 1 // optional: enables logs
+);
+ /*             
+   ns_cloner()->schedule->add(
+   $request,          // array of request data as specified above
+   time(),            // timestamp of date/time to start cloning - use time() to run immediately
+   'multisite-rest-api' // name of your project, required but used only for debugging
+);
+   */         
+        foreach ( $request as $key => $value ) {
+        ns_cloner_request()->set( $key, $value );
+        }
+
+        // Get the cloner process object.
+        $cloner = ns_cloner()->process_manager;
+
+        // Begin cloning.
+        $cloner->init();
+
+        // Check for errors (from invalid params, or already running process).
+        $errors = $cloner->get_errors();
+        if ( ! empty( $errors ) ) {
+        echo $errors;   // Handle error(s) and exit
+        }
+            
+        sleep(6);
+            
+
+
+        // Last you'll need to poll for completion to run the cleanup process
+        // when content is done cloning. Could be via AJAX to avoid timeout, or like:
+         /*do {
+            // Attempt to run finish, if content is complete.
+           $pm->maybe_finish();
+           $progress = $pm->get_progress();
+           // Pause, so we're not constantly hammering the server with progress checks.
+           sleep( 3 );
+         } while ( 'reported' !== $progress['status'] )
+
+        */     
+        //  return rest_ensure_response('reported');
+        //    sleep (3);
+
+        foreach ( $request as $key => $value ) {
+        $$key = $value;
+        }
+        $blog_details = get_blog_details($target_name,1);
+        return rest_ensure_response($blog_details);
+
+            
+        }
+    }
+        
+ 
+        
+        //Edits above
+        
       elseif (preg_match("/sites\/assign/", $route, $matches)) {
         if(! current_user_can('update_sites')){
           return rest_ensure_response('Invalid user permissions.');
